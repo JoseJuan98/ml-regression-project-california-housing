822baaf115f6271720281ecd9c30e00f7ceda805
JoseJuan98 <josejuan-98@hotmail.com>
2022-11-20 23:08:03 +0100

    update report EDA

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -17,7 +17,9 @@ from sklearn.pipeline import Pipeline
 # from src.utils import
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
-HOUSING_PATH = os.path.join('data', 'housing.csv')
+DATA_PATH = os.path.join(Path(__file__).parent.parent.parent, 'data')
+HOUSING_PATH = os.path.join(DATA_PATH, 'housing.csv')
+PREPARED_DATA = os.path.join(DATA_PATH, 'housing_prepared.csv')
 HOUSING_URL = os.path.join(DOWNLOAD_ROOT, "datasets/housing/housing.csv")
 
 
