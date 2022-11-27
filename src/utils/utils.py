4b86301bf229b29a93af9d880d5e9c4e1edb745f
JoseJuan98 <josejuan-98@hotmail.com>
2022-11-27 20:38:15 +0100

    feature: prepare data for reg analysis

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,26 +1,30 @@
 # Native libs
 import os
+import logging
 from math import ceil
 from typing import Union, Literal
 from pathlib import Path
 from urllib.request import urlretrieve
 
 # Data Analysis
-from pandas import DataFrame, read_csv, NA
-from matplotlib import pyplot
 import seaborn
+from matplotlib import pyplot
+from pandas import DataFrame, read_csv, NA, concat
 
 # Machine Learning procedures
 from sklearn.pipeline import Pipeline
 
 # Utils
 # from src.utils import
+logger = logging.getLogger('main')
+logger.addHandler(logging.StreamHandler())
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
 DATA_PATH = os.path.join(Path(__file__).parent.parent.parent, 'data')
 HOUSING_PATH = os.path.join(DATA_PATH, 'housing.csv')
 PREPARED_DATA = os.path.join(DATA_PATH, 'housing_prepared.csv')
 HOUSING_URL = os.path.join(DOWNLOAD_ROOT, "datasets/housing/housing.csv")
+TARGET = 'median_house_value'
 
 
 def fetch_housing_data(url: Union[str, Path],
@@ -158,18 +162,80 @@ def remove_outliers_iqr(dataframe: DataFrame, column: str, whisker_width: float 
         (dataframe[column] >= q1 - whisker_width * iqr) & (dataframe[column] <= q3 + whisker_width * iqr)]
 
 
-def prepare_data(data: DataFrame, target: str):
+def prepare_data(data: DataFrame, target: str, verbose: bool = False) -> DataFrame:
     """
 
     Args:
-        target:
-        data:
+        verbose (bool): if True it will print the messages, if not it won't
+        target (str): name of the target variable
+        data (pandas.DataFrame): raw data to be prepared
 
     Returns:
-
+        pandas.DataFrame: prepared data
     """
+    if verbose:
+        logger.setLevel(logging.INFO)
+    else:
+        logger.setLevel(logging.ERROR)
 
+    logger.info(f"{'':_^30} Preparing Data {'':_^30}")
 
+    from scipy.stats import boxcox
+    from sklearn.impute import SimpleImputer
+    from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+
+    # Normalizing target variable
+    old_skewness = data[target].skew()
+    data[target] = DataFrame(boxcox(data.median_house_value)[0])
+
+    logger.info(f"\t-> Target normalization. \n\t\tSkewness: \n"
+                f"{'':>30}{'Old':10}: {old_skewness:.4f}. \n"
+                f"{'':>30}{'Current':10}: {data[target].skew():.4f}\n"
+                f"\n\t-> Handling missing values. \n"
+                f"\t\tMissing values of `total_bedrooms`:\n"
+                f"{'':>30}{'Before':10}: {data.total_bedrooms.isna().sum()}")
+
+    # Missing values
+    imputer = SimpleImputer(missing_values=NA, strategy='mean')
+    data.total_bedrooms = imputer.fit_transform(X=data.total_bedrooms.to_numpy().reshape(-1, 1))
+
+    logger.info(f"{'':>30}{'After':10}: {data.total_bedrooms.isna().sum()}"
+                f"\n\n\t-> Handling outliers.")
+
+    # Handling outliers
+    variables_with_outliers = ["median_income", "total_rooms", "total_bedrooms", "population", "median_income",
+                               "housing_median_age", "households"]
+    for col in variables_with_outliers:
+        data = remove_outliers_iqr(dataframe=data, column=col, whisker_width=1.5)
+
+    # Encoding categorical variables
+    logger.info(f"\t-> Encoding categorical variables.")
+    categorical_variables = data.select_dtypes('object').columns.to_list()
+    cat_encoder = OneHotEncoder(sparse=False, handle_unknown='ignore')
+    original_index = data[categorical_variables].index
+
+    new_cat_data = cat_encoder.fit_transform(data[categorical_variables])
+    new_cat_columns = cat_encoder.get_feature_names_out().tolist()
+
+    new_cat_data = DataFrame(data=new_cat_data, columns=new_cat_columns)
+    new_cat_data.set_index(original_index, inplace=True)
+    data = concat([data, new_cat_data], axis=1, join='inner')
+    del new_cat_data, original_index
+    data.drop(columns=categorical_variables, inplace=True, axis=1)
+
+    # Scaling numerical variables
+    logger.info(f"\t-> Scaling continuous variables.\n")
+    norm_cols = [col for col in data.columns.tolist() if col not in new_cat_columns + [TARGET]]
+
+    norm_data = MinMaxScaler().fit_transform(data[norm_cols])
+    original_index = data[norm_cols].index
+
+    norm_data = DataFrame(data=norm_data, columns=norm_cols)
+    norm_data.set_index(original_index, inplace=True)
+    data[norm_cols] = norm_data[norm_cols]
+    del norm_data, original_index
+
+    return data
 
 
 def get_processing_pipeline() -> Pipeline:
